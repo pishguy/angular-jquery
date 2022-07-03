308238f59c957353882edc627f34ee6227b81597
Mahdi Pishguy <pishguy@gmail.com>
2022-07-03 09:03:34 +0430

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+declare var App: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit()
+  {
+    App.initBeforeLoad();
+    App.initCore();
+  }
 }
